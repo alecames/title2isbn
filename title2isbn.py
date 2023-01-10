22b4fd1a755a70d2fcf3d0c259b20cf6dd71f5b7
Alec Ames <alecames2001@gmail.com>
2023-01-10 13:45:44 -0500

    added multiple text file support

--- title2isbn.py
+++ title2isbn.py
@@ -1,8 +1,9 @@
 import requests
+import glob
 import sys
 
 # this script grabs the ISBN-13 of a book from the Google Books API
-# drop a line separated list of book titles in books.txt and run the script
+# drop a line separated list of book titles in a .txt file in the same directory as the script, name not important
 # alternatively, pass the titles as arguments to the script, encase in quotes and separate with a space
 
 def get_isbn(title):
@@ -13,20 +14,23 @@ def get_isbn(title):
         for identifier in industry_identifiers:
             if identifier["type"] == "ISBN_13":
                 isbn = identifier["identifier"]
-                title = book["volumeInfo"]["title"]  # get the title from the database
+                title = book["volumeInfo"]["title"]
                 return isbn, title
         isbn = industry_identifiers[0]["identifier"]
-
-        title = book["volumeInfo"]["title"]  # get the title from the database
+        title = book["volumeInfo"]["title"]
         return isbn, title
     else: return "N/A", "N/A"
 
 def read_from_file():
-    with open("titles.txt", "r") as f:
-        titles = f.readlines()
-    for line in titles:
-        entered_title = line.strip()
-        print_result(entered_title)
+    my_files = glob.glob('*.txt')
+    for file in my_files:
+        print(f"File: {file}")
+        with open(file, 'r') as f:
+            titles = f.readlines()
+        for line in titles:
+            entered_title = line.strip()
+            print_result(entered_title)
+        print()
 
 def read_from_args():
     titles = sys.argv[1:]
@@ -37,6 +41,6 @@ def print_result(title):
     isbn, title = get_isbn(title)
     print(f"{isbn}\t{title}")
 
-print("Results\nISBN\t\tTitle\n------------------------------------")
+print("------ Results ------\nISBN\t\tTitle\n------------------------------------")
 if len(sys.argv) > 1: read_from_args()
 else: read_from_file()
